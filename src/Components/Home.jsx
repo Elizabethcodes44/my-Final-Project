5592cde389b431e5f897702be71e785675097772
Elizabeth <lizbeepmc@gmail.com>
2024-02-16 01:37:15 +0300

    layout done

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css'
+import picture3 from "../assets/picture3.jpg"
+function Main() {
+    return(
+        <>
+        <main className='main'>
+            <img src ={picture3} className='picture3' alt= 'picture3'/>
+        </main>
+        </>
+    )
+}
+export default Main;
