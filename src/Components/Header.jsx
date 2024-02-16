a6fd26b1750a223f90b1c4aafe0f12c6dabddadd
Elizabeth <lizbeepmc@gmail.com>
2024-02-16 17:55:17 +0300

    header components done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,29 @@
 import "./Header.css";
+import logo from "../../public/favicon.png";
 function Header() {
   return (
-    <header className="header">
-        
-        <p>header</p>
-        
+    <header>
+      <div className="header">
+        <div className="header_left">
+          <img src={logo} className="logo" alt="logo" />
+          <h2>SHELEADS</h2>
+        </div>
+        <div className="header_center">
+          <ul className="header_list">
+            <li>Home</li>
+            <li>About</li>
+            <li>Countries</li>
+
+            <li>Gallery</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <button>DONATE</button>
+          <button>Sign-up</button>
+          <button>Log-in</button>
+        </div>
+      </div>
     </header>
   );
 }
