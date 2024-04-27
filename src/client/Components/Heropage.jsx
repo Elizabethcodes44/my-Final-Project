2e48f1f5e73cdb941afd8faa3b51d94c276cbda3
Elizabeth <lizbeepmc@gmail.com>
2024-04-27 19:50:03 +0300

    change from she leads to bump tech

--- src/client/Components/Heropage.jsx
+++ src/client/Components/Heropage.jsx
@@ -1,25 +1,27 @@
-import "./Heropage.css";
+import "./Heropage.scss";
 import image from "../../../assets/heropage2.jpg";
 
 function Heropage() {
   return (
     <>
-      <div className="hero-page" id="hero">
+      <div className="hero-page">
+        <div className="wrapper">
+          <div className="textContainer">
+            <h1>BUMP TECH</h1>
+            <p>Introducing "BumpTech" – your ultimate pregnancy app:
+
+Due Date Calculator: Get your due date instantly.
+Health Tracker: Monitor your well-being effortlessly.
+Appointment Planner: Never miss a doctor's visit.
+Tech Strength Quiz: Discover your tech interests.
+Free Tech Courses: Learn coding while expecting.</p>
+          </div>
+          </div>
         <div className="image">
           <img className="women_mount" src={image} alt="hero page image" />
         </div>
-        <div className="herotext">
-          <h3>Welcome to ``She Leads`` </h3>
-          <p>
-             🚀 We Empower Women in Tech  </p><p>💰 Learn about Financial Independence</p> <p>🩸 Learn about Menstrual
-            Hygiene and get access to free sanitary pads!</p> <p> 🤰 We care about Pregnant women.Follow us for health tips on pregnacy  &
-            Post-Delivery Care </p> <p>💆‍♀️ Women Self-Care</p> 
-            <h5>Join
-            our community by signing up on the webpage and embark on a journey of empowerment, growth, and
-            solidarity. Together, we lead the way!
-          </h5>
-         
-        </div>
+       
+       
       </div>
     </>
   );
